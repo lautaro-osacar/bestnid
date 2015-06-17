433d82b1be651b60a63e9b7f7446e7ca888085b9
Lautaro Osácar <lautaro.eo@gmail.com>
2015-06-17 19:57:24 -0300

    Definiendo estilo de perfil de usuario

--- resources/views/perfil/perfil.blade.php
+++ resources/views/perfil/perfil.blade.php
@@ -4,41 +4,17 @@
 
 <link href="{{ asset('/css/perfil.css') }}" rel="stylesheet">
 
-<nav class="navbar navbar-default" role="navigation">
-  <!-- Brand and toggle get grouped for better mobile display -->
-  <div class="navbar-header">
-    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse-2">
-      <span class="sr-only">Toggle navigation</span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </button>
-    <a class="navbar-brand" href="#">Brand</a>
+  <div id="navigation">
+    <table class="table table-hover table-bordered" cellpadding="10">
+    	<tr><td> Perfil </td></tr>
+    	<tr><td> Subastas </td></tr>
+    	<tr><td> Ofertas </td></tr>
+   	</table>
   </div>
 
-  <!-- Collect the nav links, forms, and other content for toggling -->
-  <div class="collapse navbar-collapse-2 navbar-ex1-collapse-2">
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Link</a></li>
-      <li><a href="#">Link</a></li>
-      <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Action</a></li>
-          <li><a href="#">Another action</a></li>
-          <li><a href="#">Something else here</a></li>
-          <li><a href="#">Separated link</a></li>
-          <li><a href="#">One more separated link</a></li>
-        </ul>
-      </li>
-    </ul>
-    <form class="navbar-form navbar-left" role="search">
-      <div class="form-group">
-        <input type="text" class="form-control" placeholder="Search">
-      </div>
-      <button type="submit" class="btn btn-default">Submit</button>
-    </form>
-  </div><!-- /.navbar-collapse -->
-</nav>
+  <div id="perfil-contenido">
+  <legend><center>Mi Perfil</center></legend>
+  Hola, este es juancho el perro malo
+  </div>
 
 @endsection
