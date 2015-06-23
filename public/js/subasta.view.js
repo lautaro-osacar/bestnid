330362e0e0d35376173e40c7ba51918608dfe1b6
Lautaro Osácar <lautaro.eo@gmail.com>
2015-06-23 18:59:23 -0300

    Fix otra vez

--- public/js/subasta.view.js
+++ public/js/subasta.view.js
@@ -2,7 +2,7 @@ $(document).ready( function (){
 	$("#zoom_01").elevateZoom({gallery:'gal1', cursor: 'pointer', galleryActiveClass: 'active'});
 
 	//Si aparecio un error en el alta de ofertas muestro el modal
-	if($("#error-1").text()){
+	if($("#error-0").text()){
 		$('#oferta-modal').modal({
   			backdrop: 'static',
   			keyboard: true
