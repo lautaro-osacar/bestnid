dba4a7b9bb30238db794b7cc4ce0ca97d9ceb70f
Lautaro Osácar <lautaro.eo@gmail.com>
2015-06-25 13:55:56 -0300

    Fix mensaje en ingles en login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
 				<div class="panel-body">
 					@if (count($errors) > 0)
 						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
+							<strong>Error!</strong>  Se encontraron problemas en los registros:<br><br>
 							<ul>
 								@foreach ($errors->all() as $error)
 									<li>{{ $error }}</li>
