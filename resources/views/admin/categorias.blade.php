021831559f41714f2cacd919bea24a84a11452ab
constanzaosacar <constanza.osacar@gmail.com>
2015-07-12 19:43:40 -0300

    pantalla admin->categorias

--- resources/views/admin/categorias.blade.php
+++ resources/views/admin/categorias.blade.php
@@ -6,6 +6,20 @@
 <script src="{{ asset('/js/jquery-2.1.4.min.js') }}"></script>
 
 <legend><center>Categorias</center></legend>
+
+@if (session('status'))
+    <div class="alert alert-success">
+		<span class="glyphicon glyphicon-ok"></span>&nbsp;&nbsp;{{ session('status') }}
+	</div>
+@endif
+
+<div id="create-categoria">
+	{!! Form::open(['action'=>'CategoriasController@store','class' => 'form-horizontal','id' => 'formCategoria']) !!}
+	<input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
+		<textarea id="categoria-nombre" name="categoria-nombre" rows="2"></textarea>
+		<button type="submit" class="btn btn-primary" id="categoria-btn">Crear categoria</button>
+	{!! Form::close() !!}
+</div>
 <div id="categorias" class="col-md-12">
 	<table class="table table-hover categorias-table">
 		<tr class="col-guia">
